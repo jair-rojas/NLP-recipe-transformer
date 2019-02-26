4d131367958fee923975dac6bc41b8825659291c
george.pan <georgepan98@gmail.com>
2019-02-25 23:18:19 -0600

    implementation of KB based method and ingredient finder

--- firstdraft.py
+++ firstdraft.py
@@ -8,6 +8,10 @@ Created on Sat Feb 23 16:21:29 2019
 import re
 import nltk
 import spacy
+
+
+nlp = spacy.load('en')
+
    
 class Ingredient:
     def __init__(self, qty, unit, item, comments = None, qty_option = None):
@@ -25,7 +29,7 @@ class Ingredient:
               '\ncomment: ',self.comments, '\n\n')
         
 
-ingred = ['1 1/2 pounds ground beef','1/4 cup water','1 (1 ounce) packet taco seasoning mix','12 (8 inch) flour tortillas, or more if needed', '1 (14 ounce) can refried beans','3 cups shredded Colby-Jack cheese','1 (8 ounce) jar taco sauce','1 cup shredded Colby-Jack cheese']
+ingred = ['18 medium taco shells','2 pounds lean ground beef','1 (14 ounce) bottle ketchup','1 (8 ounce) package shredded Cheese','1 large tomato, diced','1 cup iceberg lettuce, shredded']
 
 UNITWORDS = set(['can','jar','pound','ounce','cup','packet','bottle','pinch'])
 
@@ -89,22 +93,11 @@ recipe = ['Preheat oven to 375 degrees F (190 degrees C).',
           'Warm taco shells for 5 minutes on the center rack in the preheated oven.',
           'In a medium skillet over medium high heat, brown the beef. Halfway through browning, pour in ketchup. Stir well and let simmer for 5 minutes.',
           'Spoon the meat mixture into the warm taco shells and top with Cheddar cheese. Return the filled taco shells to the preheated oven and bake until cheese is melted. Top each taco with a little tomato and lettuce.']
+INGREDS = parse_ingred(ingred)
+METHODS = ['top','stir','simmer','mix','spoon','warm','preheat','bake','brown','pour','return', 'pour in']
+sortedmethods = sorted(set([i for i in METHODS]), key=len, reverse = True)
+ingred_list = sorted(set([i.item for i in INGREDS]), key=len)
 
-METHODS = ['top','stir','simmer','mix','spoon','warm','preheat','bake','brown','pour','return']
-
-sentences = []
-for i in recipe:
-    j = (i.split('.'))
-    for y in j:
-        if y == '': pass
-        else: 
-            if y[0] == ' ':
-                y = y[1:]
-            sentences.append(y)
-        
-            
-for i in sentences[1:2]:
-    doc = nlp(sentences[11])
 def pwords(doc):
     for token in doc:
         print(token.text, token.pos_, token.dep_)
@@ -120,15 +113,37 @@ def pdep(doc):
 
 # recipe assumptions: they all start with a verb OR they start with a prep phrase in which case, verb appears after the comma
 # and commas only exist if the sentence starts with a prep phrase
-def find_ingred(line, ingreds):
-    ingred_list = sorted(set([i.item for i in ingreds]), key=len)
-    num_overlap = [len(set(line.split()).intersection(set(i.split()))) for i in ingred_list]
-    if max(num_overlap) > 0:  #if we're able to match more than one word
-        return ingred_list[num_overlap.index(max(num_overlap))]  #grab the first (and thus shortest) award name that matches max(num_overlap) times
-    else:
-        return None
-    
-    
+def find_ingred(line):
+    wordoverlaps = [None] * len(line.split())
+    index = 0
+    for word in line.split():
+        num_overlap = [len(set([word]).intersection(set(i.split()))) for i in ingred_list]
+        if max(num_overlap) > 0:  #if we're able to match more than one word
+            wordoverlaps[index] = ingred_list[num_overlap.index(max(num_overlap))]  #grab the first (and thus shortest) award name that matches max(num_overlap) times
+        else:
+            wordoverlaps[index] = None
+        index += 1
+    ans = []
+    inds = []
+    for i in range(len(wordoverlaps)):
+        if wordoverlaps[i] != None:
+            ans.append(wordoverlaps[i])
+            inds.append(i)
+    return ans, inds
+        
+def find_method(line):
+    linecopy = line
+    for verb in sortedmethods:
+        line = linecopy
+        span = len(verb.split())
+        while len(line) > 0:
+            tmp = line.split()[0:span]
+            test = ' '.join(tmp).lower()
+            if test == verb:
+                return verb
+            line = ' '.join(line.split()[1:])
+    return None
+            
 
 class Step:
     def __init__(self, ingreds = None, tools=None, time=None, method=None, method_opt= None):
@@ -138,7 +153,7 @@ class Step:
         self.method=method
         self.method_opt = method_opt
 
-def parse_recipe(recipe, ingreds):
+def parse_recipe(recipe):
     sentences = []
     for i in recipe:
         j = (i.split('.'))
@@ -173,57 +188,68 @@ def parse_recipe(recipe, ingreds):
                         sent2 = sent2[:-1]
                         sent2.append(tmp + 'and ' + y)
     sentences = sent2            
-    
-#    steps = []
-#    for line in sentences: 
-#        print('working on: ', line)
-#        doc = nlp(line)
-#        nps = doc.noun_chunks
-#        verb = None
-#        ingredients = None
-#        tool = None
-#        for chunk in nps:
-#            if chunk.root.head.pos_ != 'ADP':
-#                print(chunk.root)
-#                verb = chunk.root.head.text
-#                if find_ingred(str(chunk.text), ingreds):
-#                    ingredients= (find_ingred(str(chunk.text), ingreds))
-#                else: 
-#                     tool = str(chunk.text)   
-#                break
-#        steps.append(Step(ingreds = ingredients, method = verb, tools = tool))
 
-        for line in sentences:
-            g = line.split(',')
-            splits = []
-            for j in g:
-                prev = 0
-                doc = nlp(j)
-                for i in range(len(doc)):
-                    word = doc[i]
-                    if word.pos_ == 'ADP': 
-                        splits.append(doc[prev:i])
+    splits = []
+    for line in sentences:
+        g = line.split(',')
+        split = []
+        for j in g:
+            prev = 0
+            doc = nlp(j)
+            for i in range(len(doc)):
+                word = doc[i]
+                if word.pos_ == 'ADP':
+                    if i > 0 and doc[i-1].pos_ == 'VERB': pass
+                    elif word.text.lower() != 'through':         
+                        tmp = doc[prev:i]
+                        if len(tmp) > 0:
+                            split.append(doc[prev:i])
                         prev = i
-                splits.append(doc[prev:])
-            
-            # identify the verb
-            verb = ''
+            split.append(doc[prev:])
+        splits.append(split)
+
+
+    verb = ''
+    verbs = []
+    ingredlist = []
+    for i in range(len(splits)):
+        verb = ''
+        split = splits[i]
+        for j in range(len(split)):
+            s = split[j]
+            if s[0].pos_ == 'ADP' :pass
+            else:    
+                a = find_method(s.text)
+                if a:
+                    verb = a
+                    ingred, inds = find_ingred(s.text)
+                    splits[i][j] = nlp(s.text.lower().replace(a, ' ').lstrip())
+                    for ind in inds:
+                        tmpword = s[ind].text
+                        #splits[i][j] = nlp(s.text.lower().replace(tmpword, ' ').lstrip())
+                    verbs.append(verb)
+                    ingredlist.append(ingred)
+
+        # identify the verb (in case we didnt catch it with KB)
+    for verb in verbs:
+        if verb == '':
+            ingred = []
             for s in splits:
                 ingred = []
                 if s[0].pos_ == 'ADP': pass
                 else: 
                     for j in s:
-                        
                         if j.pos_ == 'VERB':
+                            ingred = find_ingred(s.text)
                             verb = j
                             break
                         else: pass
             if str(verb) == '':
                 for s in splits:
                     if s[0].pos_ == 'ADP': pass
-                    else: verb = s[0]
-            
-            print(splits)
+                    else: 
+                        verb = s[0]
+                        ingred = find_ingred(s.text)
             verbs = [verb]
             stack = [child for child in verb.children ]
             while stack != []:
@@ -238,19 +264,49 @@ def parse_recipe(recipe, ingreds):
                             verbs.append(child)
                         for i in child.children:
                             stack.append(i)
-            print(line,'-----', verbs)
-            
-                    
-            
-            
-            
-            
-            
+            print(line,'-----', verbs, ingred)
         
+    #cleaning up        
+    splits2 = []
+    for i in splits:
+        prev = 0
+        newsplit = []
+        for j in i:
+            if j:
+                for k in range(len(j)):
+                    if j[k].pos_ == 'ADP':
+                        newsplit.append(j[prev:k])
+                        prev = k
+                newsplit.append(j[prev:])
+                print(prev)
+            else:
+                pass
+        splits2.append(newsplit)
         
+        for i in splits2:
+            for j in i:
+                if not j:
+                    i.remove(j)
+    
+    #work on method_opts
+    methodopts = [None] * len(splits2)
+    for i in range(len(splits2)):
+        tmplist = []
+        split = splits2[i]
+        for j in split:
+            for k in j:
+                if k.pos_ == 'ADP':
+                    tmplist.append(j)
+        methodopts[i] = tmplist
+    
+    steps = []
+    for i in range(len(splits2)):
+        tmpstep = Step(ingredlist[i], method = verbs[i], method_opt = methodopts[i])
+        steps.append(tmpstep)
+    
+def __init__(self, ingreds = None, tools=None, time=None, method=None, method_opt= None)    
 
 
-nlp = spacy.load('en')
 doc = nlp(u'Apple is looking at buying U.K. startup for $1 billion')
 for token in doc:
     print(token.text, token.pos_, token.dep_)    
