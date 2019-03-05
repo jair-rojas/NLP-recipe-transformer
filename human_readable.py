a61439812a8ed33569d02567063ce5197eb7d7ac
sdobon <sebastiandobon2@gmail.com>
2019-03-05 00:02:19 -0600

    clean

--- human_readable.py
+++ human_readable.py
@@ -5,8 +5,36 @@ arr2 = [ "Preheat oven to 350 degrees F (175 degrees C). Line 2 baking sheets wi
 
 from parsers2 import *
 
+# removes any words if they appear twice in a row in a step
+# we should try to use this at the very end
+# takes in all the steps and spits out all the steps removing duplicates
+def remove_dup(steps):
+    def remove_comma(string):
+        return(re.sub(',', '', string))
+
+    for s in steps:
+        for ss in s.substeps:
+            split = ss.source.split()
+            repeats = []
+            for i in range(len(split)):
+                word = split[i]
+                wordnext = ''
+                try:
+                    wordnext = split[i+1]
+                except:
+                    pass
+                if word == wordnext:
+                    repeats.append(word)
+            for i in repeats:
+                split.remove(i)
+            ss.source = ' ' + ' '.join(split)
+    return(steps)
+
+
 def reassemble(ingredients, steps):
 	ingredient_strs = []
+
+	steps = remove_dup(steps)
 
 	for i in ingredients:
 		i_str = str(i.qty) + ' '
